80705b9ee05925aa1bd4006953195b4032ef098a
José Daniel Valencia M. <josedanielvalencia019@gmail.com>
2025-04-10 21:55:12 -0600

    codigo1-taller.m

---
+++ Matlab Taller 3/codigo1-taller3.m
@@ -0,0 +1,29 @@
+% Codigo 1
+clc; clear; close all;
+
+% Definir la variable de Laplace
+ s = tf('s');
+
+% Definir las funciones de transferencia dadas en la imagen
+ G = tf([11] , [1 4]);
+ H = tf([1] , [4]);
+ K = 1 / 16;
+ 
+
+%% Paso 1: Combinación de G y K en serie
+GK = series(K,G) ; % Multiplicación de bloques en serie
+
+%% Paso 2: Simplificación de la malla de retroalimentación con H
+% Aplicar la fórmula de realimentación negativa: Geq = G / (1 + G*H)
+G_final = feedback(GK, H); % Retroalimentación negativa con H
+
+% Mostrar la función de transferencia final
+disp('La función de transferencia final Y(s)/U(s) es:');
+G_final
+
+% Respuesta al escalón unitario
+step(G_final)
+title('Dominio del tiempo a la entrada escalón unitario')
+xlabel('Tiempo (s)')
+ylabel('Salida Y(t)')
+grid on
